63e4701a275ea65f05c19e0bf24c3a71993a34a5
CPSoft.us <168373163+CPSoftUS@users.noreply.github.com>
2024-05-11 14:02:29 -0400

    Update MilageCalc.py

--- MilageCalc.py
+++ MilageCalc.py
@@ -1,9 +1,13 @@
 # Milage Calculator
-# Start Date: 04/25/2024
-# Modified Date: 04/28/2024
+# Begin Date: 04/25/2024
+# Mod.  Date: 05/11/2024
+# Version 1.1
 
-from datetime import datetime
+from datetime import datetime   #Import datetime Module.
 
+if __name__ == "__main__":      #Enterance point to the Python Program.
+
+        driversName = ""
 goalPlace = ""
 startPlace = ""
 endPlace = ""
@@ -15,15 +19,17 @@ print("")
 print("======================")
 print("Milage Calculator")
 print("By: Dan Uff")
-print("Version 1.0")
+print("Version 1.1")
 print("")
 print("(c) 2024 CPS CO.")
 print("======================")
 print("https://www.cpsoft.us/")
 print("======================")
 print("")
+
+driversName = input("Driver's FULL Name :")
 goalPlace = input("Where are you going :")
-startPlace = input("Where did you START :")
+startPlace = input("Where did you START:")
 endPlace = input("Where you STOP :")
 startMiliage = input("STARTING Milage :")
 endMilage = input("ENDING Milage :")
@@ -32,14 +38,18 @@ showDat = datetime.now()
 total = (int(startMiliage) + int(endMilage))
 
 print("===================================================")
-print("You started from: ",str(startPlace))
-print("You're going to: ",str(endPlace))
-print("Starting Milage : ",int(startMiliage))
-print("Ending Milage : ",int(endMilage))
+print("Driver's Name :",str(driversName))
+print("You're going to: ",str(goalPlace))
+print("You're coming from: ",str(startPlace))
+print("You stopped at: ",str(endPlace))
+print("Starting Milage: ",int(startMiliage))
+print("Ending Milage: ",int(endMilage))
 print("")
 print("You drove a total of ",int(total),"miles today.")
 print("===================================================")
 print("")
 print("*** END OF INFORMATION ***")
 print("")
-print("Information as of:",showDat)
+print(str(driversName)+"'s information as of:",showDat)
+print("===================================================")
+
